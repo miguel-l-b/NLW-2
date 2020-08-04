9a5dfaac126a0e77c1a5ac304bec5c4e1c9aacb5
Miguel Lopes Braido <miguelcoruj@gmail.com>
2020-08-04 17:06:24 +0000

    Add TeacherForms

--- web/src/pages/TecherForm/index.tsx
+++ web/src/pages/TecherForm/index.tsx
@@ -1,9 +1,13 @@
 import React from 'react'
 
+import PageHeader from '../../Components/PageHeader'
+
 function TeacherList() {
     return(
-        <>
-        <h1>Teacher Form</h1>
-        </>
+        <div id="page-techer-form" className="container">
+            <PageHeader title="Que incrível que você quer dar aulas." />
+        </div>
     )
 }
+
+export default TeacherList
