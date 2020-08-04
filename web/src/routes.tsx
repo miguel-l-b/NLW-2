3b1c53d90c1d5d6a1159d0f053353e7cdb90253a
Miguel Lopes Braido <miguelcoruj@gmail.com>
2020-08-04 17:07:16 +0000

    Update Route

--- web/src/routes.tsx
+++ web/src/routes.tsx
@@ -1,10 +1,18 @@
 import React from 'react'
 import { BrowserRouter, Route } from 'react-router-dom'
-import Landing from './pages/Landing'
 
-function Route() {
+import Landing from './pages/Landing'
+import TecherList from './pages/TecherList'
+import TecherForm from './pages/TecherForm'
+
+function Routes() {
+    return (
     <BrowserRouter>
-        <Route path='/' component={Landing} />
-        <Route path='/study' component={TecherList} />
+        <Route path='/' exact component={Landing} />
+        <Route path='/study' exact component={TecherList} />
+        <Route path='/give-classes' exact component={TecherForm} />
     </BrowserRouter>
+    )
 }
+
+export default Routes
