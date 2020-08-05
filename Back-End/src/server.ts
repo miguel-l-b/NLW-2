79519054a9876dc2c5fb12cd633cd4a8eaa7b70b
Miguel-Coruj <miguelcoruj@gmail.com>
2020-08-05 12:55:36 -0300

    Add Routes

--- Back-End/src/server.ts
+++ Back-End/src/server.ts
@@ -1,11 +1,12 @@
 import 'dotenv/config'
 import express, { json } from 'express'
+import routes from './Routes'
 
 const app = express()
 
 app.use(json())
 
-
+app.use(routes)
 
 app.listen(process.env.PORT)
 console.log(`Starting for http://localhost${process.env.PORT}`)
