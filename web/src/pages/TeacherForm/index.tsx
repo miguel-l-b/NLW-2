eeead7d8ace2bdce74b049cf7ffef74b3f5ba2b8
Miguel Lopes Braido <miguelcoruj@gmail.com>
2020-08-06 13:01:47 +0000

    Use TextArea and Select

--- web/src/pages/TeacherForm/index.tsx
+++ web/src/pages/TeacherForm/index.tsx
@@ -1,13 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 import PageHeader from '../../Components/PageHeader'
 import Input from '../../Components/Input'
+import Textarea from '../../Components/TextArea'
+import Select from '../../Components/Select'
 
 import warningIcon from '../../assets/images/icons/warning.svg'
 
 import './styles.css'
 
 function TeacherList() {
+
+    const [scheduleItems, setScheduleItems] = useState([
+        {week_day: 0, from: "", to: ""}
+    ])
+
+    function addNewScheduleItem() {
+        setScheduleItems([
+            ...scheduleItems,
+            { week_day: 0, from: "", to: "" }
+        ])
+    }
     return(
         <div id="page-teacher-form" className="container">
             <PageHeader 
@@ -22,12 +35,57 @@ function TeacherList() {
                     <Input name="name" label="Nome Completo" type="text" />
                     <Input name="avatar" label="Avatar" type="text" />
                     <Input name="whatsapp" label="whatsapp" type="text" />
+                    <Textarea name="bio" label="Biografia" />
                 </fieldset>
                 <fieldset>
                     <legend>Sobre a aula</legend>
 
-                    <Input name="subject" label="Matéria" type="text" />
+                    <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' }
+                        ]}
+                    />
                     <Input name="cost" label="Custo da sua hora por aula" type="text" />
+                </fieldset>
+
+                <fieldset>
+                    <legend>
+                        Horários disponíveis
+                        <button type="button" onClick={addNewScheduleItem}>
+                            + Novo horário
+                        </button>
+                    </legend>
+
+                    {scheduleItems.map(scheduleItem => {
+                        return (
+                            <div key={scheduleItem.week_day} className="schedule-item">
+                            <Select 
+                            name="week_day" 
+                            label="Dia da semana"
+                            options={[
+                                { value: '0', label: 'Domingo' },
+                                { value: '1', label: 'Segunda-feira' },
+                                { value: '2', label: 'Terça-feira' },
+                                { value: '3', label: 'Quarta-feira' },
+                                { value: '4', label: 'Quinta-feira' },
+                                { value: '5', label: 'Sexta-feira' },
+                                { value: '6', label: 'Sábado' },
+                            ]}
+                            />
+                            <Input name="from" label="Das" type="time" />
+                            <Input name="to" label="Até" type="time" />
+                            </div>
+                        )
+                    })}
                 </fieldset>
 
                 <footer>
--- web/src/pages/TeacherList/index.tsx
+++ web/src/pages/TeacherList/index.tsx
@@ -3,6 +3,7 @@ import React from 'react'
 import PageHeader from '../../Components/PageHeader'
 import TeacherItem from '../../Components/TeacherItem'
 import Input from '../../Components/Input'
+import Select from '../../Components/Select'
 
 import './styles.css'
 
@@ -11,8 +12,33 @@ function TeacherList() {
         <div id="page-teacher-list" className="container">
             <PageHeader title="Estes são os proffys disponíveis.">
                 <form id="search-teachers">
-                    <Input name="subject" label="Matéria" type="text" />
-                    <Input name="week_day" label="Dia da semana" type="text" />
+                    <Select 
+                        name="subject" 
+                        label="Matéria" 
+                        options={[
+                            { value: 'Artes', label: 'Artes' },
+                            { value: 'Biologia', label: 'Biologia' },
+                            { value: 'Física', label: 'Física' },
+                            { value: 'Química', label: 'Química' },
+                            { value: 'Matemática', label: 'Matemática' },
+                            { value: 'Português', label: 'Português' },
+                            { value: 'História', label: 'História' },
+                            { value: 'Geografia', label: 'Geografia' }
+                        ]}
+                    />
+                    <Select 
+                        name="week_day" 
+                        label="Dia da semana" 
+                        options={[
+                            { value: '0', label: 'Domingo' },
+                            { value: '1', label: 'Segunda-feira' },
+                            { value: '2', label: 'Terça-feira' },
+                            { value: '3', label: 'Quarta-feira' },
+                            { value: '4', label: 'Quinta-feira' },
+                            { value: '5', label: 'Sexta-feira' },
+                            { value: '6', label: 'Sábado' },
+                        ]}
+                    />
                     <Input name="time" label="Hora" type="time" />
                 </form>
             </PageHeader>
